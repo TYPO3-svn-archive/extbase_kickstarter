93142cfb06078033806c15d34470268ee99bbf77
stoefln <stoefln@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-28 16:09:01 +0000

    test extension: added domain model box
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extension_editing@33670 735d13b6-9817-0410-8766-e36946ffe9aa

--- test_extension/ext_tables.php
+++ test_extension/ext_tables.php
@@ -13,4 +13,27 @@ t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'test extensio
 //t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
 
 
+t3lib_extMgm::addLLrefForTCAdescr('tx_testextension_domain_model_box','EXT:test_extension/Resources/Private/Language/locallang_csh_tx_testextension_domain_model_box.xml');
+t3lib_extMgm::allowTableOnStandardPages('tx_testextension_domain_model_box');
+$TCA['tx_testextension_domain_model_box'] = array (
+	'ctrl' => array (
+		'title'             => 'LLL:EXT:test_extension/Resources/Private/Language/locallang_db.xml:tx_testextension_domain_model_box',
+		'label' 			=> 'width',
+		'tstamp' 			=> 'tstamp',
+		'crdate' 			=> 'crdate',
+		'versioningWS' 		=> 2,
+		'versioning_followPages'	=> TRUE,
+		'origUid' 			=> 't3_origuid',
+		'languageField' 	=> 'sys_language_uid',
+		'transOrigPointerField' 	=> 'l18n_parent',
+		'transOrigDiffSourceField' 	=> 'l18n_diffsource',
+		'delete' 			=> 'deleted',
+		'enablecolumns' 	=> array(
+			'disabled' => 'hidden'
+			),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/Tca.php',
+		'iconfile' 			=> t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_testextension_domain_model_box.gif'
+	)
+);
+
 ?>
