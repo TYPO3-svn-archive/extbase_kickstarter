65148aa21458acb2a04529d448c0299146e3293a
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-02 14:39:26 +0000

    [+TASK] Built up basic structure
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35269 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/js/layout.js
+++ Resources/Public/js/layout.js
@@ -1,3 +1,11 @@
+Ext.ns("T3.ExtbaseKickstarter");
+T3.ExtbaseKickstarter.DocumentHeader = Ext.extend(Ext.Panel, {
+	title: 'Doc header',
+	region: 'north',
+	html: 'I\'m a doc header',
+});
+Ext.reg('T3.ExtbaseKickstarter.DocumentHeader', T3.ExtbaseKickstarter.DocumentHeader);
+
 Ext.onReady(function(){
 	Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
 
@@ -5,44 +13,60 @@ Ext.onReady(function(){
 		layout: 'border',
 		items: [
 			{
-				region: 'east',
-				id: 'east-panel',
-				title: 'Properties',
-				collapsible: true,
-				split: true,
-				width: 225,
-				minSize: 175,
-				maxSize: 400,
-				margins: '0 5 0 0',
-				layout: {
-					type: 'accordion',
-					animate: true
-				},
-				items: [{
-					title: 'General',
-					border: false,
-					iconCls: 'nav'
-				}, {
-					title: 'Settings',
-					border: false,
-					iconCls: 'settings'
-				}]
-			},
-			{
 				region: 'west',
-				title: 'Extension tree',
-				id: 'extension-tree',
 				split: true,
 				width: 200,
 				minSize: 175,
 				maxSize: 400,
-				collapsible: true,
-				margins: '0 0 0 5'
+				//collapsible: true,
+				margins: '0 0 0 5',
+				items: [
+					{
+						xtype: 'T3.ExtbaseKickstarter.DocumentHeader'
+					},
+					{
+						title: 'Extension Tree',
+						region: 'south',
+						id: 'extension-tree',
+					},
+				]
 			},
 			{
 				region: 'center',
 				collapsible: false,
-				margins: '0 0 0 0'
+				margins: '0 0 0 0',
+				items: [
+					{
+						xtype: 'T3.ExtbaseKickstarter.DocumentHeader'
+					},
+					{
+						region: 'south',
+						layout: 'hbox',
+						items: [
+							{
+								title: 'Main Content',
+								region: 'west',
+								html: 'I\'m an area',
+								flex: 1,
+								height: 500
+							},
+							{
+								region: 'east',
+								id: 'east-panel',
+								title: 'Properties',
+								width: 150,
+								collapsible: true,
+								split: true,
+								width: 225,
+								height: 500,
+								minSize: 175,
+								maxSize: 400,
+								margins: '0 5 0 0',
+								html: 'Properties'
+							}
+						]
+					}
+				]
 			}
 		]
 	});
