9a2f0703123aa707c67c24278f6f2f0fb91aaab4
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-02 13:45:31 +0000

    [~TASK] Trying to get the mockup to work..
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35260 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/js/layout.js
+++ Resources/Public/js/layout.js
@@ -3,62 +3,62 @@ Ext.onReady(function(){
 
 	var viewport = new Ext.Viewport({
 		layout: 'border',
-		items: [{
+		items: [
+			{
 				region: 'north',
-				contentEl: 'north-div',
 				collapsible: false,
 				margins: '0 0 0 0',
 				tbar: mainToolbar
-		}, {
-			region: 'south',
-			contentEl: 'south-div',
-			split: true,
-			height: 100,
-			minSize: 100,
-			maxSize: 200,
-			collapsible: true,
-			title: 'Console',
-			margins: '0 0 0 0'
-		}, {
-			region: 'east',
-			id: 'east-panel',
-			title: 'Properties',
-			collapsible: true,
-			split: true,
-			width: 225,
-			minSize: 175,
-			maxSize: 400,
-			margins: '0 5 0 0',
-			layout: {
-				type: 'accordion',
-				animate: true
 			},
-			items: [{
-				contentEl: 'east-div1',
-				title: 'General',
-				border: false,
-				iconCls: 'nav'
-			}, {
-				title: 'Settings',
-				contentEl: 'east-div2',
-				border: false,
-				iconCls: 'settings'
-			}]
-		}, {
-			region: 'west',
-			contentEl: 'west-div',
-			title: 'Extension tree',
-			split: true,
-			width: 200,
-			minSize: 175,
-			maxSize: 400,
-			collapsible: true,
-			margins: '0 0 0 5'
-		}, {
-			region: 'center',
-			contentEl: 'center-div',
-			collapsible: false,
-			margins: '0 0 0 0'
-		}]
+			{
+				region: 'south',
+				split: true,
+				height: 100,
+				minSize: 100,
+				maxSize: 200,
+				collapsible: true,
+				title: 'Console',
+				margins: '0 0 0 0'
+			},
+			{
+				region: 'east',
+				id: 'east-panel',
+				title: 'Properties',
+				collapsible: true,
+				split: true,
+				width: 225,
+				minSize: 175,
+				maxSize: 400,
+				margins: '0 5 0 0',
+				layout: {
+					type: 'accordion',
+					animate: true
+				},
+				items: [{
+					title: 'General',
+					border: false,
+					iconCls: 'nav'
+				}, {
+					title: 'Settings',
+					border: false,
+					iconCls: 'settings'
+				}]
+			},
+			{
+				region: 'west',
+				title: 'Extension tree',
+				split: true,
+				width: 200,
+				minSize: 175,
+				maxSize: 400,
+				collapsible: true,
+				margins: '0 0 0 5'
+			},
+			{
+				region: 'center',
+				collapsible: false,
+				margins: '0 0 0 0'
+			}
+		]
 	});
 });
