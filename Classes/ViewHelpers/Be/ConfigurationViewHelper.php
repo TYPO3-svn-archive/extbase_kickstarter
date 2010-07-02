75c777f1c78d43d305f91b9fa03e800bb1432920
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-02 13:15:55 +0000

    [+TASK] Added JS and CSS Files for Mockup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35258 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/Be/ConfigurationViewHelper.php
+++ Classes/ViewHelpers/Be/ConfigurationViewHelper.php
@@ -6,11 +6,18 @@ class Tx_ExtbaseKickstarter_ViewHelpers_Be_ConfigurationViewHelper extends Tx_Fl
 		$doc->bodyTagAdditions .= 'class="yui-skin-sam"';
 
 		$pageRenderer = $doc->getPageRenderer();
-
+		
 		// SECTION: JAVASCRIPT FILES
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/toolbar.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/layout.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/extensionTree.js');
 		
 		// ExtJS Application
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/Application.js');
+		//$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/Application.js');
+		
+		// SECTION: CSS FILES
+		$pageRenderer->addCssFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/css/sprites.css');
+		$pageRenderer->addCssFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/css/style.css');
 
 	}
 }
