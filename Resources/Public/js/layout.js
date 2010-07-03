855594b594e9a1a9ea83343270b0af0050232dbe
radmiraal <radmiraal@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-03 07:29:12 +0000

    ~ Changed the layout to look more like the page module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35284 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/js/layout.js
+++ Resources/Public/js/layout.js
@@ -1,114 +1,65 @@
 Ext.ns("T3.ExtbaseKickstarter");
-T3.ExtbaseKickstarter.DocumentHeader = Ext.extend(Ext.Panel, {
-});
+T3.ExtbaseKickstarter.DocumentHeader = Ext.extend(Ext.Panel, {});
 Ext.reg('T3.ExtbaseKickstarter.DocumentHeader', T3.ExtbaseKickstarter.DocumentHeader);
 
-T3.ExtbaseKickstarter.ViewPort = Ext.extend(Ext.Viewport, {
-	
-	initComponent: function() {
-		Ext.Viewport.superclass.initComponent.call(this);
-
-		this.el = Ext.get(this.el) || Ext.getBody();
-        if (this.el.dom === document.body) {
-	        this.el.dom.parentNode.className += ' x-viewport';
-        }
-        this.el.setHeight = Ext.emptyFn;
-        this.el.setWidth = Ext.emptyFn;
-        this.el.setSize = Ext.emptyFn;
-        this.el.dom.scroll = 'no';
-        this.allowDomMove = false;
-        this.autoWidth = true;
-        this.autoHeight = true;
-        Ext.EventManager.onWindowResize(this.fireResize, this);
-        this.renderTo = this.el;
+var extbaseKickstarterMainToolbar = [{
+	text: 'Action Menu',
+	handler: function() {
+		console.log('Action menu');
 	}
-});
+}, {
+	text: 'Action Menu item 2',
+	handler: function() {
+		console.log('Action menu item 2');
+	}
+}];
 
+var extbaseKickstarterEastAccordionTabs = [{
+	title: 'General',
+	border: false,
+	iconCls: 'nav'
+}, {
+	title: 'Settings',
+	border: false,
+	iconCls: 'settings'
+}];
 
 Ext.onReady(function(){
 	Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
 
-	var viewport = new T3.ExtbaseKickstarter.ViewPort({
+	var viewport = new Ext.Viewport({
 		layout: 'border',
-		el: 'ExtbaseKickstarter',
-		defaults:	{
-			collapsible:	true,
-			collapseMode:	'mini',
-			split:			true
+		defaults: {
+			collapsible:	false,
+			split:			true,
+			margins:		'0 0 0 0'
 		},
-		items: [
-			{
-				region: 'west',
-				width: 200,
-				minSize: 175,
-				maxSize: 400,
-				//collapsible: true,
-				margins: '0 0 0 5',
-				items: [
-					{
-	//					title: 'Doc header',
-						region: 'north',
-						items: [
-							{
-								xtype: 'button',
-								text: 'Add something',
-								handler: function() {
-									console.log('yeah');
-								}
-							}
-						]
-					},
-					{
-	//					title: 'Extension Tree',
-						region: 'south',
-						id: 'extension-tree'
-					},
-				]
+		items: [{
+			region:			'north',
+			xtype:			'panel',
+			tbar:			extbaseKickstarterMainToolbar
+		}, {
+			region:			'west',
+			collapseMode:	'mini',
+			width:			280,
+			minWidth:		175,
+			maxWidth:		300,
+			items: [{
+				id:			'extension-tree'
+			}]
+		}, {
+			region:			'center',
+			id:				'content'
+		}, {
+			region:			'east',
+			collapseMode:	'mini',
+			split:			true,
+			width:			225,
+			layout: {
+				type:		'accordion',
+				animate:	true
 			},
-			{
-				region: 'center',
-				collapsible: false,
-				margins: '0 0 0 0',
-				items: [
-					{
-//						title: 'Doc header',
-						region: 'north'
-						/*,items: [
-							{
-								title: 'test',
-								region: 'center'
-							}
-						]*/
-					},
-					{
-						region: 'south',
-						layout: 'hbox',
-						items: [
-							{
-								title: 'Main Content',
-								region: 'west',
-								html: 'I\'m an area',
-								flex: 1,
-								height: 500
-							},
-							{
-								region: 'east',
-								id: 'east-panel',
-								title: 'Properties',
-								width: 150,
-								collapsible: true,
-								split: true,
-								width: 225,
-								height: 500,
-								minSize: 175,
-								maxSize: 400,
-								margins: '0 5 0 0',
-								html: 'Properties'
-							}
-						]
-					}
-				]
-			}
-		]
+			items:			extbaseKickstarterEastAccordionTabs
+		}]
 	});
 });
