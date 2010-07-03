f8bb2952e2a59f69e336b98e069948657a7922b5
sebastian <sebastian@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-03 11:40:35 +0000

    [+TASK] ExtbaseKickstarter: Started with initialization procedure.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35293 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/Be/ConfigurationViewHelper.php
+++ Classes/ViewHelpers/Be/ConfigurationViewHelper.php
@@ -8,9 +8,18 @@ class Tx_ExtbaseKickstarter_ViewHelpers_Be_ConfigurationViewHelper extends Tx_Fl
 		$pageRenderer = $doc->getPageRenderer();
 		
 		// SECTION: JAVASCRIPT FILES
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/toolbar.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Application.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Application/AbstractBootstrap.js');
+
+		// now the loading order does not matter anymore
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/DomainModelling/Bootstrap.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/DomainModelling/DomainModeller.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/Bootstrap.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/Layout.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/TabLayout.js');
+		/*$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/toolbar.js');
 		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/layout.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/extensionTree.js');
+		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/js/extensionTree.js');*/
 		
 		// ExtJS Application
 		//$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/Application.js');
