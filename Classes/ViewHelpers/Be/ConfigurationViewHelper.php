56b96f447f06f6bf9b6eadced58ddcef81fa278f
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-03 11:41:29 +0000

    [-TASK] Removed old YUI body class
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35294 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/Be/ConfigurationViewHelper.php
+++ Classes/ViewHelpers/Be/ConfigurationViewHelper.php
@@ -3,7 +3,6 @@
 class Tx_ExtbaseKickstarter_ViewHelpers_Be_ConfigurationViewHelper extends Tx_Fluid_ViewHelpers_Be_AbstractBackendViewHelper {
 	public function render() {
 		$doc = $this->getDocInstance();
-		$doc->bodyTagAdditions .= 'class="yui-skin-sam"';
 
 		$pageRenderer = $doc->getPageRenderer();
 		
