2153d8a96e5ba28255f9a798af83de3aafd716fa
radmiraal <radmiraal@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-03 08:09:35 +0000

    ~ Render the layout in docbody (including dirty resize for docbody size)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35285 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/js/layout.js
+++ Resources/Public/js/layout.js
@@ -1,34 +1,48 @@
 Ext.ns("T3.ExtbaseKickstarter");
-T3.ExtbaseKickstarter.DocumentHeader = Ext.extend(Ext.Panel, {});
-Ext.reg('T3.ExtbaseKickstarter.DocumentHeader', T3.ExtbaseKickstarter.DocumentHeader);
+//T3.ExtbaseKickstarter.DocumentHeader = Ext.extend(Ext.Panel, {});
+//Ext.reg('T3.ExtbaseKickstarter.DocumentHeader', T3.ExtbaseKickstarter.DocumentHeader);
 
-var extbaseKickstarterMainToolbar = [{
-	text: 'Action Menu',
-	handler: function() {
-		console.log('Action menu');
-	}
-}, {
-	text: 'Action Menu item 2',
-	handler: function() {
-		console.log('Action menu item 2');
-	}
-}];
+T3.ExtbaseKickstarter.ViewPort = Ext.extend(Ext.Viewport, {
+	initComponent: function() {
+		Ext.Viewport.superclass.initComponent.call(this);
+		this.el = Ext.get(this.el) || Ext.getBody();
 
-var extbaseKickstarterEastAccordionTabs = [{
-	title: 'General',
-	border: false,
-	iconCls: 'nav'
-}, {
-	title: 'Settings',
-	border: false,
-	iconCls: 'settings'
-}];
+        if (this.el.dom === document.body) {
+
+	        this.el.dom.parentNode.className += ' x-viewport';
+
+        }
+
+        this.el.setHeight = Ext.emptyFn;
+
+        this.el.setWidth = Ext.emptyFn;
+        this.el.setSize = Ext.emptyFn;
+
+        this.el.dom.scroll = 'no';
+
+        this.allowDomMove = false;
+
+        this.autoWidth = true;
+        this.autoHeight = true;
+
+        Ext.EventManager.onWindowResize(this.fireResize, this);
+        this.renderTo = this.el;
+	}
+});
+Ext.reg('T3.ExtbaseKickstarter.ViewPort', T3.ExtbaseKickstarter.ViewPort);
 
 Ext.onReady(function(){
-	Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+//	Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
 
-	var viewport = new Ext.Viewport({
+	/** Quick and dirty :-) **/
+	var docbody = Ext.get('typo3-inner-docbody');
+	docbody.setWidth('100%').setHeight('100%');
+	docbody.setHeight(docbody.getHeight() - 7).setWidth(docbody.getWidth() - 6);
+
+
+	var viewPort = new T3.ExtbaseKickstarter.ViewPort({
 		layout: 'border',
+		el: 'typo3-inner-docbody',
 		defaults: {
 			collapsible:	false,
 			split:			true,
@@ -37,7 +51,17 @@ Ext.onReady(function(){
 		items: [{
 			region:			'north',
 			xtype:			'panel',
-			tbar:			extbaseKickstarterMainToolbar
+			tbar:			[{
+				text: 'Action Menu',
+				handler: function() {
+					console.log('Action menu');
+				}
+			}, {
+				text: 'Action Menu item 2',
+				handler: function() {
+					console.log('Action menu item 2');
+				}
+			}]
 		}, {
 			region:			'west',
 			collapseMode:	'mini',
@@ -59,7 +83,15 @@ Ext.onReady(function(){
 				type:		'accordion',
 				animate:	true
 			},
-			items:			extbaseKickstarterEastAccordionTabs
+			items:			[{
+				title: 'General',
+				border: false,
+				iconCls: 'nav'
+			}, {
+				title: 'Settings',
+				border: false,
+				iconCls: 'settings'
+			}]
 		}]
 	});
 });
