5b5e42bce4171c1c78c3c80ab45179834c39c151
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-04 20:36:47 +0000

    [~TASK] Reworked the ConfigurationViewHelper.php so it will be extensible in the future. (Not quite sure if it's solid yet.)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extjsui@35336 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/Be/ConfigurationViewHelper.php
+++ Classes/ViewHelpers/Be/ConfigurationViewHelper.php
@@ -1,28 +1,98 @@
 <?php
 
 class Tx_ExtbaseKickstarter_ViewHelpers_Be_ConfigurationViewHelper extends Tx_Fluid_ViewHelpers_Be_AbstractBackendViewHelper {
+	
+	/**
+	 * @var string Absolute Path to the folder where the Editor Packages reside
+	 */
+	private $defaultEditorPackagePath;
+	
+	/**
+	 * @var t3lib_PageRenderer
+	 */
+	private $pageRenderer;
+	
+	public function __construct() {
+		$this->defaultEditorPackagePath = t3lib_extMgm::extPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Packages/';
+		$this->pageRenderer = $this->getDocInstance()->getPageRenderer();
+	}
+	
 	public function render() {
-		$pageRenderer = $this->getDocInstance()->getPageRenderer();
 		
 		// SECTION: JAVASCRIPT FILES
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Application.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Application/AbstractBootstrap.js');
+		$this->pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Application.js');
+		$this->pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Application/AbstractBootstrap.js');
 
 		// now the loading order does not matter anymore
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Packages/Welcome/Bootstrap.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Packages/Welcome/WelcomeScreen.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Packages/General/Bootstrap.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Packages/General/General.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Packages/DomainModelling/Bootstrap.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/Packages/DomainModelling/DomainModeller.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/Bootstrap.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/Layout.js');
-		$pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/TabLayout.js');
+		$this->addJSPackageFiles();
+		$this->pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/Bootstrap.js');
+		$this->pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/Layout.js');
+		$this->pageRenderer->addJsFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/JavaScript/UserInterface/TabLayout.js');
 		
 		// SECTION: CSS FILES
-		$pageRenderer->addCssFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/css/sprites.css');
-		$pageRenderer->addCssFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/css/style.css');
+		$this->pageRenderer->addCssFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/css/sprites.css');
+		$this->pageRenderer->addCssFile(t3lib_extMgm::extRelPath('extbase_kickstarter') . 'Resources/Public/css/style.css');
 
+	}
+	
+	private function addJSPackageFiles() {
+		$packages = $this->getJSPackageConfiguration();
+		foreach($packages as $package => $config) {
+			if(isset($config['packagesBaseDirectory'])) {
+				$baseDirectory = $config['packagesBaseDirectory'] . $package . '/';
+			} else {
+				$baseDirectory = $this->defaultEditorPackagePath . $package . '/';
+			}
+			try {
+				$iterator = $this->iterateDirectoryRecursively($baseDirectory);
+			} catch(Exception $e) {
+				// Directory was not found/accessible or something..
+				continue;
+			}
+			foreach ($iterator as $file) {
+				if($this->isJSFile($file->getFileName())) {
+					$this->pageRenderer->addJsFile($file->getPathname());
+				}
+			}
+		}
+	}
+	
+	private function isJSFile($filepath) {
+		$parts = explode('.', $filepath);
+		if($parts[count($parts)-1] === 'js') {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	/**
+	 * Iterate over a directory with all subdirectories
+	 *
+	 * (Taken from TYPO3 Phoenix)
+	 *
+	 * @param string $directory The directory to iterate over
+	 * @return Iterator An iterator
+	 * @author Christopher Hlubek
+	 */
+	protected function iterateDirectoryRecursively($directory) {
+		return new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
+	}
+	
+	private function getJSPackageConfiguration() {
+		// TODO: This array has to be configurable from outside
+		$packages = array(
+			'Welcome' => array(),
+			'General' => array(),
+			'DomainModelling' => array(),
+			/*
+			 * Including packages from EXT:myext/Resources/Public/JavaScript/ExtbaseKickstarterPackages/MyPackage/ would work like this:
+			'MyPackage' => array(
+				'packagesBaseDirectory' => 'EXT:myext/Resources/Public/JavaScript/ExtbaseKickstarterPackages/'
+			),
+			*/
+		);
+		return $packages;
 	}
 }
 
