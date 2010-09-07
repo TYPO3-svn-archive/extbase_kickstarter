5f4c20d5d45930cc517a8eebfad0093e90347486
nicodh <nicodh@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-07 15:34:01 +0000

    [+FEATURE] Default values, typeHints and passByReference in method parameters implemented
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_kickstarter/branches/extension_editing@37790 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/MethodViewHelper.php
+++ Classes/ViewHelpers/MethodViewHelper.php
@@ -24,8 +24,44 @@ class Tx_ExtbaseKickstarter_ViewHelpers_MethodViewHelper extends Tx_Fluid_Core_V
 		$parameters = array();
 		
 		foreach($methodSchemaObject->getParameters()  as $parameter){
-			$parameters[] = strtolower($parameter->getVarType()).' $'.$parameter->getName();
-			t3lib_div::devLog($methodSchemaObject->getName().':'.$parameter->getName(), 'parameter debug');			
+			$parameterName = $parameter->getName();
+			$typeHint = $parameter->getTypeHint ();
+			
+			if($parameter->isOptional()){
+				$defaultValue = $parameter->getDefaultValue();
+				if(!empty($typeHint)){
+					// typeHints of optional parameter have the format "typeHint or defaultValue"
+					$typeHintParts = explode(' ',$typeHint);
+					$typeHint = $typeHintParts[0];
+				}
+				
+				if(is_array($defaultValue)){
+					if(!empty($defaultValue)){
+						$defaultValue = json_encode($defaultValue);
+						if(strpos($defaultValue,'}')>-1){
+							$defaultValue = str_replace('{','array(',$defaultValue);
+							$defaultValue = str_replace('}',')',$defaultValue);
+							$defaultValue = str_replace(':',' => ',$defaultValue);
+						}
+						if(strpos($defaultValue,']')>-1){
+							$defaultValue = str_replace('[','array(',$defaultValue);
+							$defaultValue = str_replace(']',')',$defaultValue);
+						}
+						//t3lib_div::devLog('default Value: '. $defaultValue, 'parameter debug');				
+					}
+					else $defaultValue = 'array()';
+				}
+				else {
+					$defaultValue = json_encode($defaultValue);
+				}
+				$parameterName .= ' = '.$defaultValue;
+			}
+			$parameterName = '$'.$parameterName;
+			if($parameter->isPassedByReference()){
+				$parameterName = '&'.$parameterName;
+			}
+			$parameters[] = $typeHint.' '.$parameterName;
+			//t3lib_div::devLog($methodSchemaObject->getName().':'.$parameter->getName(), 'parameter debug');			
 		}
 		return implode(', ',$parameters);
 	}
